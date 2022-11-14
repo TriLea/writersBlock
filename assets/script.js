da6f0bd0b3642fc9b1a1f29af5819de2894f9459
justinaypangit <just10gv@gmail.com>
2022-11-14 15:04:08 -0800

    Merged from main

--- assets/script.js
+++ assets/script.js
@@ -27,28 +27,3 @@ async function currentWordFav() {
 
 
 // var favoritesList = JSON.parse(localStorage.getItem(favorites));
-
-
-
-
-function onSubmit(event) {
-    event.preventDefault();
-    console.log("Form has been submitted");
-    const input = document.getElementById("input");
-    console.log(input.value);
-}
-
-document.getElementById("favorites-button").addEventListener("click", currentWordFav);
-
-function currentWordFav() {
-    var input = document.getElementById("input");
-    localStorage.setItem("favorites", input.value);
-    console.log(input.value + " has been favorited!");
-}
-
-for (var i = 0, local = localStorage.length; i < local; ++i) {
-	var elements = document.createElement("p");
-	elements.textContent = localStorage.getItem(localStorage.key(i.value));
-	console.log("Displaying word: " + i);
-	output.appendChild(elements);
-}
