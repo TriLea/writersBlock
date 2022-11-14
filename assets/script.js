fd375bd8a3ca0760fa692556ece49a6f1623fffd
trista_lea <tristan_lea@outlook.com>
2022-11-13 21:16:41 -0800

    parallax and merge

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
-  });
+});
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log("Form has been submitted");
+    const input = document.getElementById("input");
+    console.log(input.value);
+}
+
+document.getElementById("favorites-button").addEventListener("click", currentWordFav);
+
+function currentWordFav() {
+    var input = document.getElementById("input");
+    localStorage.setItem("favorites", input.value);
+    console.log(input.value + " has been favorited!");
+}
+
+for (var i = 0, local = localStorage.length; i < local; ++i) {
+	var elements = document.createElement("p");
+	elements.textContent = localStorage.getItem(localStorage.key(i.value));
+	console.log("Displaying word: " + i);
+	output.appendChild(elements);
+}
