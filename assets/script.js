2e39a22fe84936438b42566fb294bc9ff11d9a96
trista_lea <tristan_lea@outlook.com>
2022-11-14 17:48:00 -0800

    removed image

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+/*
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
 });
+*/
 
 function onSubmit(event) {
     event.preventDefault();
